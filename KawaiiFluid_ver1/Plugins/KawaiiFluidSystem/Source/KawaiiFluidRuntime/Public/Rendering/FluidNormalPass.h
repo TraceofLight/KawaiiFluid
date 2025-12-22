7cf6cb4f515938e9b559d3d7e36d5fd1e25c8f32
heejun_kim <rickyjun96@gmail.com>
2025-12-22 16:01:06 +0900

    feat: normal pass - 기존에 누락된 파일 추가
    [git-p4: depot-paths = "//depot/KawaiiFluid_ver1/": change = 51]

---
+++ KawaiiFluid_ver1/Plugins/KawaiiFluidSystem/Source/KawaiiFluidRuntime/Public/Rendering/FluidNormalPass.h
@@ -0,0 +1,18 @@
+// Copyright KawaiiFluid Team. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RenderGraphDefinitions.h"
+
+class FSceneView;
+
+/**
+ * Normal Reconstruction Pass
+ * Reconstructs world-space normals from the smoothed depth buffer.
+ */
+void RenderFluidNormalPass(
+	FRDGBuilder& GraphBuilder,
+	const FSceneView& View,
+	FRDGTextureRef SmoothedDepthTexture,
+	FRDGTextureRef& OutNormalTexture);
